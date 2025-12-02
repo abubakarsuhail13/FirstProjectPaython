5b5fb4df2ac9c17bbae5f2814efb9ccf3eadb7a5
abubakarsuhail13 <abubakarsuhail13@gmail.com>
2025-12-02 19:31:02 +0500

    Functions Automation projectFile

---
+++ Test/Python_BasicsFunctions.py
@@ -0,0 +1,18 @@
+def printhello():
+     print("hello")
+
+printhello()
+
+def printhi(name = "Sohail"):
+    print("Hello, " + name)
+printhi("Sunny")
+printhi()
+def sum(a,b,c):
+    print(a+b+c)
+sum(10, 20, 30)
+
+"""This is the function of the calculations some of 2 or 3 numbers!"""
+def ReturnSum(a,b,c):
+    return (a+b+c)
+x = ReturnSum(5,10,15)
+print(x)
