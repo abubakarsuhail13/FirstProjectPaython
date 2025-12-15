1f87a1a927b973b635c34ea11afdd8440578a0a5
abubakarsuhail13 <abubakarsuhail13@gmail.com>
2025-12-15 21:14:09 +0500

    Functions Automation projectFile

--- Test/Python_Basic_Collection_Dictionary.py
+++ Test/Python_Basic_Collection_Dictionary.py
@@ -1,1 +1,28 @@
+my_dict = {
+    "Class" : "IT",
+    "Name" : "Python",
+    "Age" : 30
+}
 
+print(my_dict)
+print(my_dict["Name"])
+print(my_dict["Age"])
+print(my_dict.get("Class"))
+print(my_dict.values())
+
+for x in my_dict:
+    print(my_dict[x])
+
+for x,y in my_dict.items():
+    print(x,y)
+
+my_dict ["IT"] = "Meta physics"
+print(my_dict)
+
+my_dict ["Color"] = "Red"
+print(my_dict)
+my_dict.popitem()
+print(my_dict)
+
+del my_dict ["Age"]
+print(my_dict)
